5a6e29042688debe8bc25b330bf906401ed3bc37
bollo-omar <bolloaggrey@gmail.com>
2022-11-16 11:30:45 -0800

    pushing to repository

--- _atoi.c
+++ _atoi.c
@@ -2,12 +2,10 @@
 
 /**
  * interactive - returns true if shell is interactive mode
- *
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
-*/
-
+ */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
@@ -15,13 +13,10 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
- *
  * @c: the char to check
  * @delim: the delimeter string
- *
  * Return: 1 if true, 0 if false
-*/
-
+ */
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -31,12 +26,10 @@ int is_delim(char c, char *delim)
 }
 
 /**
- * _isalpha - checks for alphabetic character
- *
- * @c: The character to input
- *
- * Return: 1 if c is alphabetic, 0 otherwise
-*/
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
 
 int _isalpha(int c)
 {
@@ -47,12 +40,10 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
- *
- * @s: the string to be converted
- *
- * Return: 0 if no numbers in string, converted number otherwise
-*/
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
 
 int _atoi(char *s)
 {
